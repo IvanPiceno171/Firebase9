a6b12421a3772350b1b69c1c58b22497740c48ad
unknown <you@example.com>
2022-11-07 10:16:16 -0800

    login&logout

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 
 
-import {initializeApp} from 'firebase/app'
+import { initializeApp } from 'firebase/app'
 import {
-    getFirestore, collection, onSnapshot, 
+    getFirestore, collection, onSnapshot,
     addDoc, deleteDoc, doc,
     query, where,
     orderBy, serverTimestamp,
     getDoc, updateDoc
 } from 'firebase/firestore'
-// import { env } from 'process';
+
+//authentication
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword, 
+    signOut,
+    onAuthStateChanged
+} from 'firebase/auth'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 //grabbed from firebase website project settings
@@ -20,13 +28,14 @@ const firebaseConfig = {
     messagingSenderId: "354306728672",
     appId: "1:354306728672:web:d3ffa1f5bcccc73935a685"
     // measurementId: "G-KS5T7P4JGV"
-  };
+};
 
 // init firebase app
 initializeApp(firebaseConfig)
 
 //init services
 const db = getFirestore()
+const auth = getAuth()
 
 // collection ref
 const colRef = collection(db, 'books')
@@ -36,55 +45,55 @@ const colRef = collection(db, 'books')
 const q = query(colRef, orderBy('createdAt'))
 
 // real time collection data
-onSnapshot(q, (snapshot)=>{
-      let books = [];
-      // loops snapshot.docs and adds data() method
-      //...doc is the spread operator
+onSnapshot(q, (snapshot) => {
+    let books = [];
+    // loops snapshot.docs and adds data() method
+    //...doc is the spread operator
     //   console.log(snapshot.docs)
-      snapshot.docs.forEach((doc)=>{
-          // pushes to empty books array
-          // grabs doc data and doc id
-          books.push({...doc.data(), id: doc.id})
-      })
-      console.log(books)
+    snapshot.docs.forEach((doc) => {
+        // pushes to empty books array
+        // grabs doc data and doc id
+        books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books)
 
-  })
+})
 
 
-  // add form eventlisterner
+// add form eventlisterner
 const addBookForm = document.querySelector('.add')
 addBookForm.addEventListener('submit', addBook)
-function addBook(event){
-    event.preventDefault(); 
-    
+function addBook(event) {
+    event.preventDefault();
+
     addDoc(colRef, {
         title: addBookForm.title.value,
         author: addBookForm.author.value,
         createdAt: serverTimestamp()
     })
-    .then(()=>{
-        // resets input fields to blank
-      addBookForm.reset();
-    })
+        .then(() => {
+            // resets input fields to blank
+            addBookForm.reset();
+        })
 
 }
 
 // delete document
 const deleteBookForm = document.querySelector('.delete');
-deleteBookForm.addEventListener('submit', (e)=>{
+deleteBookForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     // 3 parameters 
     // db initilzation
     // 'books' name of database
     // grabs id from database and deletes value
-    const docRef = doc(db, 'books', deleteBookForm.id.value) 
-    
+    const docRef = doc(db, 'books', deleteBookForm.id.value)
+
     //async can add then & catch 
     deleteDoc(docRef)
-    .then(()=>{
-        //resets delete input to blank
-        deleteBookForm.reset()
+        .then(() => {
+            //resets delete input to blank
+            deleteBookForm.reset()
         })
 })
 
@@ -92,7 +101,7 @@ deleteBookForm.addEventListener('submit', (e)=>{
 //single document
 const docRef = doc(db, 'books', 'p3PY6LHVgGITvCI2LhMe')
 
-onSnapshot(docRef, (doc) =>{
+onSnapshot(docRef, (doc) => {
     // data method to grab data
     // doc.id logs documents id
     console.log(doc.data(), doc.id)
@@ -102,13 +111,71 @@ onSnapshot(docRef, (doc) =>{
 const updateForm = document.querySelector('.update')
 updateForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    
+
     let docRef = doc(db, 'books', updateForm.id.value)
-    
+
     updateDoc(docRef, {
         title: 'updated title'
     })
-    .then(() => {
-        updateForm.reset()
+        .then(() => {
+            updateForm.reset()
+        })
+})
+
+
+// signing users up
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    // .email & .password are input name
+    // <input name = 'email'>
+    const email = signupForm.email.value
+    // <input name = 'password'>
+    const password = signupForm.password.value
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(cred => {
+            console.log('user created:', cred.user)
+            signupForm.reset()
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+})
+
+// logging in and out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+    signOut(auth)
+        .then(() => {
+            console.log('user signed out')
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+})
+
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user logged in:', cred.user)
+      loginForm.reset()
     })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+
+// subscribing to auth changes
+onAuthStateChanged(auth, (user) =>{
+    console.log('user status changed', user)
 })
